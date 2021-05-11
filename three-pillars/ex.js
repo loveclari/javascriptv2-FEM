5d708b00a7a882842e96c96429f688eab26a6604
loveclari <loveclari>
2021-05-11 16:33:12 -0700

    changes

---
+++ three-pillars/ex.js
@@ -0,0 +1,55 @@
+class Bookshelf {
+	constructor() {
+		this.favoriteBooks = [];
+	}
+
+	// TODO: define methods `addFavoriteBook(..)`
+	// and `printFavoriteBooks()`
+
+	addFavoriteBook(bookName) {
+		if (!bookName.includes("Great")) {
+			this.favoriteBooks.push(bookName);
+		}
+	}
+
+	printFavoriteBooks() {
+		console.log(`Favorite Books: ${String(this.favoriteBooks.length)}`);
+		for (let bookName of this.favoriteBooks) {
+			console.log(bookName);
+		}
+	}
+
+}
+
+
+function loadBooks(Bookshelf) {
+	// TODO: call fakeAjax( .. );
+
+	fakeAjax(BOOK_API, function myBooks(bookNames) {
+		for (let bookName of bookNames) {
+			Bookshelf.addFavoriteBook(bookName)
+		}
+
+	});
+}
+
+var BOOK_API = "https://some.url/api";
+var mynewBooks = new Bookshelf();
+loadBooks(mynewBooks);
+
+
+
+// ***********************
+
+// NOTE: don't modify this function at all
+function fakeAjax(url,cb) {
+	setTimeout(function fakeLoadingDelay(){
+		cb([
+			"A Song of Ice and Fire",
+			"The Great Gatsby",
+			"Crime & Punishment",
+			"Great Expectations",
+			"You Don't Know JS"
+		]);
+	},500);
+}
